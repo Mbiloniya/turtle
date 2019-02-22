c6e773592e6dbc0b7db2e46e40fa036adc7291da
mbiloniya <mahesh@darkbears.com>
2019-02-22 15:18:07 +0530

    changes made in rockPaper.py file

--- rockPaper.py
+++ rockPaper.py
@@ -1,13 +1,7 @@
 import random
 user1 = input("Hi,my name is :")
 user2 = input("hi %s,you are playing with computer"%user1)
-for i in range(10):
-    user1_choice = input("%s,do you pick any one in between rock,paper and scissor"%user1)
-    user2_choice = random.choice(["rock","paper","scissor"])
-    if user1_choice == "exit":
-        break
-    else:
-        def compare(u1,u2):
+def compare(u1,u2):
             if u1 == u2:
                 print("Tie")
             elif u1 == "rock":
@@ -27,6 +21,10 @@ for i in range(10):
                     print("winner is computer")
             else:
                 print("you enter a wrong choice")
-
-    # print(compare(user1_choice,user2_choice))
-    compare(user1_choice,user2_choice)
+for i in range(10):
+    user1_choice = input("%s,do you pick any one in between rock,paper and scissor"%user1)
+    user2_choice = random.choice(["rock","paper","scissor"])
+    if user1_choice == "exit":
+        break
+    else:
+        compare(user1_choice,user2_choice)
